4479152625ea2462550899a2ac086423cb98317e
Itay Gershman <itayge@wix.com>
2022-02-06 13:58:47 +0200

    changes in the app according to the PR

--- 1-Vanilla/main.js
+++ 1-Vanilla/main.js
@@ -1,231 +1,245 @@
-const list = [
+(function () {
+  // const list = []
+  const list = [
     {
       text: "Doing the Dishes",
       status: "unfinished",
-      isEditMode: false,
       id: 1,
     },
     {
       text: "Fold Laundry",
       status: "unfinished",
-      isEditMode: false,
       id: 2,
     },
     {
       text: "Build Todo App",
       status: "finished",
-      isEditMode: false,
       id: 3,
     },
     {
       text: "Get a haircut",
       status: "unfinished",
-      isEditMode: false,
       id: 4,
     },
     {
       text: "Call to mom and dad",
       status: "unfinished",
-      isEditMode: false,
       id: 5,
     },
     {
       text: "Game day against Elitzur Tel-Aviv",
       status: "unfinished",
-      isEditMode: false,
       id: 6,
     },
     {
       text: "Make dinner",
       status: "unfinished",
-      isEditMode: false,
       id: 7,
     },
     {
       text: "Eat lunch",
       status: "finished",
-      isEditMode: false,
       id: 8,
     },
     {
       text: "Go to Wix offices",
       status: "unfinished",
-      isEditMode: false,
       id: 9,
     },
     {
       text: "Watch Deni Avdjia game",
       status: "unfinished",
-      isEditMode: false,
       id: 10,
     },
     {
       text: "Go to step 2",
       status: "unfinished",
-      isEditMode: false,
       id: 11,
     },
     {
       text: "Infra webinar at 3",
       status: "unfinished",
-      isEditMode: false,
       id: 12,
     },
     {
       text: "Pay the bills",
       status: "unfinished",
-      isEditMode: false,
       id: 13,
     },
     {
       text: "Contribute code to open source",
       status: "unfinished",
-      isEditMode: false,
       id: 14,
     },
     {
       text: "Finish Velo site",
       status: "unfinished",
-      isEditMode: false,
       id: 15,
     },
   ];
-  
+
+  const editModeItems = [];
+
+  const ul = document.getElementById("todoList");
+
   const loadToDoList = (list) => {
-    const ul = document.getElementById("todoList");
     ul.innerHTML = "";
     list.forEach((item) => {
       ul.appendChild(createItem(list, item));
     });
   };
-  
+
   const addTodo = () => {
-    const inputText = document.getElementById("addTodo").value;
-    list.push({
-      text: inputText,
+    const inputText = document.getElementById("addTodo");
+    const newTodo = {
+      text: inputText.value,
       status: "unfinished",
       id: list.length + 1,
-    });
-    loadToDoList(list);
+    };
+    list.push(newTodo);
+    ul.insertBefore(createItem(list, newTodo), ul.firstChild);
+    inputText.value = ""; //clear input text
   };
-  
+
+  // Remove the todo item from the list and from the DOM
   const removeTodo = (list, item) => {
     list = list.filter(function (ele) {
       return ele.id != item.id;
     });
-  
-    loadToDoList(list);
+    const li = document.getElementById(item.id);
+    li.remove();
   };
-  
-  const editTodo = (list, item) => {
-    list = list.map((ele) => {
-      if (ele.id === item.id) {
-        ele.isEditMode = !ele.isEditMode;
+
+  function removeElement(array, elem) {
+    var index = array.indexOf(elem);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  }
+
+  const renderComponent = (elem, curr, modifiedElem) => {
+    elem.firstChild.replaceChild(curr, modifiedElem);
+  };
+
+  const editTodo = (item) => {
+    const li = document.getElementById(item.id);
+    const isFound = editModeItems.some((element) => {
+      if (element.id === item.id) {
+        return true;
       }
-      return ele;
     });
-    loadToDoList(list);
+
+    const elemToChange = li.firstChild.children[1];
+    if (isFound) {
+      removeElement(editModeItems, item);
+      const todo = document.createElement("span");
+      const editedText = document.getElementById(`${item.id}-editTodo`);
+      todo.innerText = editedText.value;
+      todo.classList.add("todo-text");
+      if (item.status === "finished") {
+        todo.classList.add("finished-todo");
+        li.firstChild.checked = true;
+      }
+      renderComponent(li, todo, elemToChange);
+    } else {
+      editModeItems.push(item);
+      const inputText = document.createElement("input");
+      inputText.type = "text";
+      inputText.value = item.text;
+      inputText.classList.add("todo-text");
+      inputText.classList.add("edit-todo");
+      inputText.id = `${item.id}-editTodo`;
+      renderComponent(li, inputText, elemToChange);
+    }
   };
-  
+
   const toggleTodo = (item) => {
     const li = document.getElementById(item.id);
-    const text = li.children[0].children[1];
+    const text = li.firstChild.children[1];
     if (item.status === "finished") {
-      text.style["text-decoration"] = "none";
+      text.classList.add("unfinished-todo");
       item.status = "unfinished";
     } else {
-      text.style["text-decoration"] = "line-through";
+      text.classList.add("finished-todo");
       item.status = "finished";
     }
   };
-  
+
   const createButton = (cb, icon, className) => {
     const button = document.createElement("button");
-    button.innerHTML = `<i class="${icon}" aria-hidden="true"></i>`;
+    const iTag = document.createElement("i");
+    iTag.className = icon;
+    button.append(iTag);
     button.classList.add(className);
     button.onclick = cb;
     button.style.cursor = "pointer";
     return button;
   };
-  
+
   const createItem = (list, item) => {
     const li = document.createElement("li");
     li.id = item.id;
-  
+
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.onclick = () => toggleTodo(item);
-  
-    const spanText = document.createElement("span");
-    spanText.classList.add("spanText");
-    spanText.appendChild(checkbox);
-  
-    if (item.isEditMode) {
-      //edit model
-      const inputText = document.createElement("input");
-      inputText.type = "text";
-      inputText.value = item.text;
-      inputText.classList.add("edit-todo");
-      inputText.id = `${item.id}-editTodo`;
-      spanText.appendChild(inputText);
-    } else {
-      //text mode
-      const text = document.createElement("span");
-      text.innerText = item.text;
-      text.style.marginLeft = "20px";
-      if (item.status === "finished") {
-        text.style["text-decoration"] = "line-through";
-        checkbox.checked = true;
-      }
-      spanText.appendChild(text);
+
+    const listItemText = document.createElement("div");
+    listItemText.classList.add("list-item-text");
+    listItemText.appendChild(checkbox);
+
+    const text = document.createElement("span");
+    text.innerText = item.text;
+    text.classList.add("todo-text");
+    if (item.status === "finished") {
+      text.classList.add("finished-todo");
+      checkbox.checked = true;
     }
-  
+    listItemText.appendChild(text);
+
     const spanButton = document.createElement("span");
-    spanButton.classList.add("spanActions");
-  
+    spanButton.classList.add("list-item-actions");
+
     const removeButton = createButton(
       () => removeTodo(list, item),
       "fa fa-trash",
       "button"
     );
     const editButton = createButton(
-      () => editTodo(list, item),
+      () => editTodo(item),
       "fa fa-pencil",
       "edit"
     );
     spanButton.appendChild(removeButton);
     spanButton.appendChild(editButton);
-  
-    li.appendChild(spanText);
+
+    li.appendChild(listItemText);
     li.appendChild(spanButton);
     return li;
   };
-  
-  const editListenerHandler = () => {
-    const text = e.target.parentNode.children[1];
+
+  const editListenerHandler = (e) => {
+    const text = e.target;
     const id = Number(text.id.split("-")[0]);
-    let updatedList = list.map((ele) => {
+    let updatedItem = list.find((ele) => {
       if (ele.id === id) {
         ele.text = text.value;
-        ele.isEditMode = false;
+        return true;
       }
-      return ele;
     });
-    loadToDoList(updatedList);
+    editTodo(updatedItem);
   };
-  
-  (function () {
-    document.querySelector("#addTodo").addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-        addTodo();
-      }
-    });
-  
-    document.querySelector("#todoList").addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-          editListenerHandler()
-      }
-    });
-    loadToDoList(list);
-  })();
-  
+
+  document.querySelector("#addTodo").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  });
+
+  document.querySelector("#todoList").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      editListenerHandler(e);
+    }
+  });
+  loadToDoList(list);
+})();
