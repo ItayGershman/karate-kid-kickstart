bbfb3f977c3ddb7f2c47a779ebe8d3435feda276
Itay Gershman <itayge@wix.com>
2022-03-30 10:57:26 +0300

    move TodoApi.ts file to more proper location in the folder tree

--- 5-Cookies/client-react/src/Contexts/TodoContext.ts
+++ 5-Cookies/client-react/src/Contexts/TodoContext.ts
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import { ITodoApi } from "../interfaces/interfaces";
-import { TodosAPI } from "../TodosAPI";
+import { TodosAPI } from "../API/TodosAPI";
 
 export const todosApi = new TodosAPI();
 export const TodosApiContext = createContext<ITodoApi>(todosApi);
